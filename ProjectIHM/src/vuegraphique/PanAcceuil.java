9b05f51fc3c024397995a1e7522d1815415d82c9
Unknown <Anwarfatayri1997@gmail.com>
2018-05-06 16:46:14 +0200

    pan acceuil fini

--- ProjectIHM/src/vuegraphique/PanAcceuil.java
+++ ProjectIHM/src/vuegraphique/PanAcceuil.java
@@ -22,7 +22,8 @@ public class PanAcceuil extends JPanel  {
 	
 	
 		
-		JLabel JTexteAccueil = new JLabel();
+		JLabel JtexteAccueil = new JLabel();
+		JLabel JTexteNotificcation = new JLabel();
 		JLabel JNotificcation = new JLabel();
 		JLabel JNextNotif  = new JLabel();
 		JLabel JAtHome  = new JLabel();
@@ -31,11 +32,13 @@ public class PanAcceuil extends JPanel  {
 		
 		
 		Box boxgauche  = Box.createVerticalBox();
-		Box boxdroite  = Box.createVerticalBox();
+		Box boxdroite  = Box.createHorizontalBox();
 		Box boxTextAcceuil  = Box.createHorizontalBox();
+		Box boxTextNotif  = Box.createHorizontalBox();
 		Box boxNotification  = Box.createHorizontalBox();
 		Box boxNextNotif  = Box.createHorizontalBox();
 		
+		int notif;
 		
 		
 		public void initialisation() {
@@ -44,107 +47,132 @@ public class PanAcceuil extends JPanel  {
 			        //notif1
 					BufferedImage notif1=null;
 					try {
-						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+						notif1 = ImageIO.read(new File("RESSOURCE/notif1.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
 					ImageIcon notif1a=new ImageIcon(notif1);
 					
-					 //notif1
-					BufferedImage notif1=null;
+					 //notif2
+					BufferedImage notif2=null;
 					try {
-						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+						notif2 = ImageIO.read(new File("RESSOURCE/notif2.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon notif1a=new ImageIcon(notif1);
+					ImageIcon notif2a=new ImageIcon(notif2);
 					
-					 //notif1
-					BufferedImage notif1=null;
+					 //notif3
+					BufferedImage notif3=null;
 					try {
-						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+						notif3 = ImageIO.read(new File("RESSOURCE/notif3.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon notif1a=new ImageIcon(notif1);
+					ImageIcon notif3a=new ImageIcon(notif3);
 					
-					 //notif1
-					BufferedImage notif1=null;
+				
+					
+					 //notif4
+					BufferedImage notif4=null;
 					try {
-						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+						notif4 = ImageIO.read(new File("RESSOURCE/notif4.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon notif1a=new ImageIcon(notif1);
+					ImageIcon notif4a=new ImageIcon(notif4);
 					
-					 //notif1
-					BufferedImage notif1=null;
+					
+					//notif5
+					BufferedImage notif5=null;
 					try {
-						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+						notif5 = ImageIO.read(new File("RESSOURCE/notif5.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon notif1a=new ImageIcon(notif1);
+					ImageIcon notif5a=new ImageIcon(notif5);
 					
-					
-					//CADENAS FERMEE
-					BufferedImage cadenas_ferme=null;
+					//notif6
+					BufferedImage notif6=null;
 					try {
-						cadenas_ferme = ImageIO.read(new File("RESSOURCE/cadenasFerme.png"));
+						notif6 = ImageIO.read(new File("RESSOURCE/notif6.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon cadenas_ferme2=new ImageIcon(cadenas_ferme);
+					ImageIcon notif6a=new ImageIcon(notif6);
 					
-					
-					//ALARM
-					BufferedImage alarm=null;
+					//next
+					BufferedImage next=null;
 					try {
-						alarm = ImageIO.read(new File("RESSOURCE/alarm.png"));
+						next = ImageIO.read(new File("RESSOURCE/next.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon alarm1=new ImageIcon(alarm);
+					ImageIcon next1=new ImageIcon(next);
 					
-					//ALARM DISABLED
-					BufferedImage alarmDisabled=null;
+					//at home
+					BufferedImage atHome=null;
 					try {
-						alarmDisabled = ImageIO.read(new File("RESSOURCE/alarmeDisabled.png"));
+						atHome = ImageIO.read(new File("RESSOURCE/atHome.png"));
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}		
-					ImageIcon alarmDisabled1=new ImageIcon(alarmDisabled);
+					ImageIcon atHome1=new ImageIcon(atHome);
 					
+					//pourcentage safety
+					BufferedImage pourcentageSafety=null;
+					try {
+						pourcentageSafety = ImageIO.read(new File("RESSOURCE/pourcentageSafety.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon pourcentageSafety1=new ImageIcon(pourcentageSafety);
+					
+					
+					
+					
+					//TEXTE ACCEUIL
+					JtexteAccueil.setText("WELCOME");
+					JtexteAccueil.setFont(new Font("Tahoma", Font.BOLD, 32));
+					
+					//TEXTE NOTIFICATIONS
+					JTexteNotificcation.setText("Notifications :");
+					JTexteNotificcation.setFont(new Font("Tahoma", Font.BOLD, 15));
 			
-					//CADENAS
+					//NOTIFICATIONS
 					
-					Jcadenas.setIcon(cadenas_ouvert2);
-					JTexteEtat.setText("Your house is not armed");
-					JTexteEtat.setFont(new Font("Tahoma", Font.BOLD, 32));
-					JTexteEtat2.setText(" ");
-					JTexteEtat2.setFont(new Font("Tahoma", Font.BOLD, 32));
-					
-					ouvert=true;
-					
-					Jcadenas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//� ajouter et mettre pour chaque label
-					Jcadenas.addMouseListener(new MouseListener() {
+					JNextNotif.setIcon(next1);
+					JNotificcation.setIcon(notif1a);		
+					notif=1;
+					JNextNotif.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//� ajouter et mettre pour chaque label
+					JNextNotif.addMouseListener(new MouseListener() {
 						@Override
 						public void mouseReleased(MouseEvent e) {
 						}
 						@Override
 						public void mousePressed(MouseEvent e) {
-							if(ouvert==true) {
-							Jcadenas.setIcon(cadenas_ferme2);
-							JTexteEtat.setText("EVRYTHING LOOKS GOOD");
-							JTexteEtat2.setText("Your home is armed");
-							ouvert=false;
-							}
-							else {
-				     		Jcadenas.setIcon(cadenas_ouvert2);
-							JTexteEtat.setText("Your home is not armed");
-							JTexteEtat2.setText(" ");
-							ouvert=true;
-							}
+						    if(notif==1) {
+						    	JNotificcation.setIcon(notif2a);
+						    	notif=2;
+						    }else
+						    	if(notif==2) {
+						    		JNotificcation.setIcon(notif3a);
+							    	notif=3;
+							    }else
+							    	if(notif==3) {
+							    		JNotificcation.setIcon(notif4a);
+								    	notif=4;
+								    }else
+								    	if(notif==4) {
+								    		JNotificcation.setIcon(notif5a);
+									    	notif=5;
+									    }else
+									    	if(notif==5) {
+									    		JNotificcation.setIcon(notif6a);
+									    		JNextNotif.setVisible(false);
+										    	notif=6;
+										    }
+						    	
 						}
 			            @Override
 			            public void mouseEntered(MouseEvent e) {
@@ -160,64 +188,34 @@ public class PanAcceuil extends JPanel  {
 						}
 					});
 					
-					//Alarm
-				    enabled=false;
-				    Jalarme.setIcon(alarmDisabled1);
-					JTexteVide.setText("         ");
-					JTexteAlarme.setText("Alarme Disabled");
-					JTexteAlarme.setFont(new Font("Tahoma", Font.BOLD, 27));
+					//AT HOME
+					JAtHome.setIcon(atHome1);
 					
-					Jalarme.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//� ajouter et mettre pour chaque label
-					Jalarme.addMouseListener(new MouseListener() {
-						@Override
-						public void mouseReleased(MouseEvent e) {
-						}
-						@Override
-						public void mousePressed(MouseEvent e) {
-							if(enabled==false) {
-								Jalarme.setIcon(alarm1);
-								JTexteAlarme.setText("Alarme Enabled");
-								enabled=true;
-							}
-							else {
-								JTexteAlarme.setText("Alarme Disabled");
-								Jalarme.setIcon(alarmDisabled1);
-								enabled=false;
-							}
-							
-						}
-			            @Override
-			            public void mouseEntered(MouseEvent e) {
-			            }
-
-			            @Override
-			            public void mouseExited(MouseEvent e) {
-			            	
-			            }
-						@Override
-						public void mouseClicked(MouseEvent e) {
-
-						}
-					});
-					boxTextEtat.add(Box.createRigidArea(new Dimension(0,180)));
-					boxTextEtat.add(JTexteEtat);
-					boxTextEtat2.add(JTexteEtat2);
-					boxTextEtat2.add(Box.createRigidArea(new Dimension(0,130)));
-					boxTextAlarme.add(Jalarme);
-					boxTextAlarme.add(JTexteVide);
-					boxTextAlarme.add(JTexteAlarme);
-					boxgauche.add(boxTextEtat);
-					boxgauche.add(boxTextEtat2);
-					boxgauche.add(boxTextAlarme);
+					//POURCENTAGE SAFETY
+					JPourcentageSafety.setIcon(pourcentageSafety1);
+				   
+					boxTextAcceuil.add(Box.createRigidArea(new Dimension(0,0)));
+					boxTextAcceuil.add(JtexteAccueil);
+					boxTextAcceuil.add(Box.createRigidArea(new Dimension(0,250)));
+					boxTextNotif.add(JTexteNotificcation);
+					boxNotification.add(JNotificcation);
+					boxNextNotif.add(JNextNotif);
+					
+					boxgauche.add(boxTextAcceuil);
+					boxgauche.add(boxTextNotif);
+					boxgauche.add(boxNotification);
+					boxgauche.add(boxNextNotif);
 					boxgauche.setVisible(true);
-					boxdroite.add(Jcadenas);
+					
+					boxdroite.add(JAtHome);
+					boxdroite.add(JPourcentageSafety);
 					boxdroite.setVisible(true);
 					this.add(boxgauche);
 					this.add(boxdroite);
 					
 					
 		
-		
+				
 		
 
 	}
