c75f63823350bbc7cf621c8328676bd1ce305a6a
Unknown <Anwarfatayri1997@gmail.com>
2018-05-06 20:58:30 +0200

    up

--- ProjectIHM/src/vuegraphique/PanCamera.java
+++ ProjectIHM/src/vuegraphique/PanCamera.java
@@ -1,6 +1,7 @@
 package vuegraphique;
 
 import java.awt.Color;
+import java.io.File;
 
 import javax.swing.JPanel;
 
@@ -10,6 +11,25 @@ public class PanCamera extends JPanel  {
 	public void initialisation() {
 		// TODO Auto-generated method stub
 		this.setBackground(Color.WHITE);
+		 File vlcInstallPath = new File("root/vlc");
+	     EmbeddedMediaPlayer player;
+
+	     public PanCamera(){
+	       
+			private static final long serialVersionUID = 1L;
+
+			NativeLibrary.addSearchPath("libvlc", vlcInstallPath.getAbsolutePath());
+	         EmbeddedMediaPlayerComponent videoCanvas = new EmbeddedMediaPlayerComponent();
+	         this.setLayout(new BorderLayout());
+	         this.add(videoCanvas, BorderLayout.CENTER);
+	         this.player = videoCanvas.getMediaPlayer();
+	     }
+
+	     public void play(String media) {
+	         player.prepareMedia(media);
+	         player.parseMedia();
+	         player.play();
+	     }
 		
 
 	}
