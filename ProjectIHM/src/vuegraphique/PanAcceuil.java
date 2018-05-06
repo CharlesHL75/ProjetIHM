b0afa049d86eded7bf759712da08cf27e86145c7
Unknown <Anwarfatayri1997@gmail.com>
2018-05-06 16:05:11 +0200

    up

--- ProjectIHM/src/vuegraphique/PanAcceuil.java
+++ ProjectIHM/src/vuegraphique/PanAcceuil.java
@@ -1,11 +1,17 @@
 package vuegraphique;
 
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,9 +20,203 @@ public class PanAcceuil extends JPanel  {
 	private static final long serialVersionUID = 4721795520307721056L;
 	
 	
-	public void initialisation() {
-		// TODO Auto-generated method stub
-		this.setBackground(Color.WHITE);
+	
+		
+		JLabel JTexteAccueil = new JLabel();
+		JLabel JNotificcation = new JLabel();
+		JLabel JNextNotif  = new JLabel();
+		JLabel JAtHome  = new JLabel();
+		JLabel JPourcentageSafety  = new JLabel();
+		
+		
+		
+		Box boxgauche  = Box.createVerticalBox();
+		Box boxdroite  = Box.createVerticalBox();
+		Box boxTextAcceuil  = Box.createHorizontalBox();
+		Box boxNotification  = Box.createHorizontalBox();
+		Box boxNextNotif  = Box.createHorizontalBox();
+		
+		
+		
+		public void initialisation() {
+		        	this.setBackground(Color.WHITE);
+			
+			        //notif1
+					BufferedImage notif1=null;
+					try {
+						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon notif1a=new ImageIcon(notif1);
+					
+					 //notif1
+					BufferedImage notif1=null;
+					try {
+						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon notif1a=new ImageIcon(notif1);
+					
+					 //notif1
+					BufferedImage notif1=null;
+					try {
+						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon notif1a=new ImageIcon(notif1);
+					
+					 //notif1
+					BufferedImage notif1=null;
+					try {
+						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon notif1a=new ImageIcon(notif1);
+					
+					 //notif1
+					BufferedImage notif1=null;
+					try {
+						notif1 = ImageIO.read(new File("RESSOURCE/cadenasOuvert.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon notif1a=new ImageIcon(notif1);
+					
+					
+					//CADENAS FERMEE
+					BufferedImage cadenas_ferme=null;
+					try {
+						cadenas_ferme = ImageIO.read(new File("RESSOURCE/cadenasFerme.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon cadenas_ferme2=new ImageIcon(cadenas_ferme);
+					
+					
+					//ALARM
+					BufferedImage alarm=null;
+					try {
+						alarm = ImageIO.read(new File("RESSOURCE/alarm.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon alarm1=new ImageIcon(alarm);
+					
+					//ALARM DISABLED
+					BufferedImage alarmDisabled=null;
+					try {
+						alarmDisabled = ImageIO.read(new File("RESSOURCE/alarmeDisabled.png"));
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+					ImageIcon alarmDisabled1=new ImageIcon(alarmDisabled);
+					
+			
+					//CADENAS
+					
+					Jcadenas.setIcon(cadenas_ouvert2);
+					JTexteEtat.setText("Your house is not armed");
+					JTexteEtat.setFont(new Font("Tahoma", Font.BOLD, 32));
+					JTexteEtat2.setText(" ");
+					JTexteEtat2.setFont(new Font("Tahoma", Font.BOLD, 32));
+					
+					ouvert=true;
+					
+					Jcadenas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//� ajouter et mettre pour chaque label
+					Jcadenas.addMouseListener(new MouseListener() {
+						@Override
+						public void mouseReleased(MouseEvent e) {
+						}
+						@Override
+						public void mousePressed(MouseEvent e) {
+							if(ouvert==true) {
+							Jcadenas.setIcon(cadenas_ferme2);
+							JTexteEtat.setText("EVRYTHING LOOKS GOOD");
+							JTexteEtat2.setText("Your home is armed");
+							ouvert=false;
+							}
+							else {
+				     		Jcadenas.setIcon(cadenas_ouvert2);
+							JTexteEtat.setText("Your home is not armed");
+							JTexteEtat2.setText(" ");
+							ouvert=true;
+							}
+						}
+			            @Override
+			            public void mouseEntered(MouseEvent e) {
+			            }
+
+			            @Override
+			            public void mouseExited(MouseEvent e) {
+			            	
+			            }
+						@Override
+						public void mouseClicked(MouseEvent e) {
+
+						}
+					});
+					
+					//Alarm
+				    enabled=false;
+				    Jalarme.setIcon(alarmDisabled1);
+					JTexteVide.setText("         ");
+					JTexteAlarme.setText("Alarme Disabled");
+					JTexteAlarme.setFont(new Font("Tahoma", Font.BOLD, 27));
+					
+					Jalarme.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));//� ajouter et mettre pour chaque label
+					Jalarme.addMouseListener(new MouseListener() {
+						@Override
+						public void mouseReleased(MouseEvent e) {
+						}
+						@Override
+						public void mousePressed(MouseEvent e) {
+							if(enabled==false) {
+								Jalarme.setIcon(alarm1);
+								JTexteAlarme.setText("Alarme Enabled");
+								enabled=true;
+							}
+							else {
+								JTexteAlarme.setText("Alarme Disabled");
+								Jalarme.setIcon(alarmDisabled1);
+								enabled=false;
+							}
+							
+						}
+			            @Override
+			            public void mouseEntered(MouseEvent e) {
+			            }
+
+			            @Override
+			            public void mouseExited(MouseEvent e) {
+			            	
+			            }
+						@Override
+						public void mouseClicked(MouseEvent e) {
+
+						}
+					});
+					boxTextEtat.add(Box.createRigidArea(new Dimension(0,180)));
+					boxTextEtat.add(JTexteEtat);
+					boxTextEtat2.add(JTexteEtat2);
+					boxTextEtat2.add(Box.createRigidArea(new Dimension(0,130)));
+					boxTextAlarme.add(Jalarme);
+					boxTextAlarme.add(JTexteVide);
+					boxTextAlarme.add(JTexteAlarme);
+					boxgauche.add(boxTextEtat);
+					boxgauche.add(boxTextEtat2);
+					boxgauche.add(boxTextAlarme);
+					boxgauche.setVisible(true);
+					boxdroite.add(Jcadenas);
+					boxdroite.setVisible(true);
+					this.add(boxgauche);
+					this.add(boxdroite);
+					
+					
+		
 		
 		
 
